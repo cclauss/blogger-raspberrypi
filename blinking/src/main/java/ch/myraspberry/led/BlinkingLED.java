ba10da74d58b5834461b426b228a10a22d46903d
Markus Keller <goofyworld88@gmail.com>
2015-06-20 11:07:19 +0200

    Added main class and pom

---
+++ blinking/src/main/java/ch/myraspberry/led/BlinkingLED.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package ch.myraspberry.led;
+
+public final class BlinkingLED {
+
+	public BlinkingLED() {
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
