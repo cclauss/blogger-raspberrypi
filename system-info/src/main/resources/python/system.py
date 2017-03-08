65523d8a5d3b18e3ca12723828f4a0689a083e66
cclauss <cclauss@bluewin.ch>
2017-03-08 16:09:30 +0100

    Optimized code

--- system-info/src/main/resources/python/system.py
+++ system-info/src/main/resources/python/system.py
@@ -1,10 +1,8 @@
 import json
 from system_info import SystemInfo
 
-data = {}
 h1 = SystemInfo()
-data["cpu_temperature"] = h1.cpu_temperature
-data["disk_space"] = h1.disk_space
-data["cpu_use"] = h1.cpu_use
-data["ram_info"] = h1.ram_info
-print(json.dumps(data))
+print(json.dumps({"cpu_temperature": h1.cpu_temperature,
+                  "disk_space": h1.disk_space,
+                  "cpu_use": h1.cpu_use,
+                  "ram_info": h1.ram_info}))
